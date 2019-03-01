a973ac2df680efdc444168c95e601a9eb64263e8
sliim35 <slim.r35@gmail.com>
2019-03-01 22:04:58 +0300

    Global styles

---
+++ frontend/source/styles/global.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  a {
+    text-decoration: none;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`;
---
+++ frontend/source/styles/theme.js
@@ -0,0 +1,5 @@
+export const theme = {
+  white: '#FFF',
+  borderGrey: '#EEE',
+  navGrey: '#F8F8FA',
+};
