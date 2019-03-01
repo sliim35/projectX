fc3c75be8edde69421f175f2a38a5227e67f59f4
sliim35 <slim.r35@gmail.com>
2019-03-01 22:05:09 +0300

    App component

---
+++ frontend/source/components/side-nav/SideNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const SideNav = () => {
+  return (
+    <div>
+      It is SideNav
+      <a href="#">Hello!</a>
+    </div>
+  );
+};
--- frontend/source/index.jsx
+++ frontend/source/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('app'));
+import { App } from './components/App/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
