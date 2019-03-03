d1d4964983f6f7f1eb71855982483a6392576a4c
sliim35 <slim.r35@gmail.com>
2019-03-03 20:03:59 +0300

    Content component

--- frontend/source/layout/content/Content.jsx
+++ frontend/source/layout/content/Content.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { Footer } from '../footer/Footer';
-
 import { ContentStyled } from './styles/ContentStyled';
 
 export const Content = () => {
   return (
     <ContentStyled>
-      <Footer />
+      This is content!!!!!!!!! Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Aliquid sint quibusdam corrupti itaque amet dolorum, eos
+      commodi voluptate reiciendis voluptas?
     </ContentStyled>
   );
 };
--- frontend/source/layout/content/styles/ContentStyled.js
+++ frontend/source/layout/content/styles/ContentStyled.js
@@ -4,4 +4,5 @@ export const ContentStyled = styled.main`
   display: flex;
   margin-left: 240px;
   margin-top: 64px;
+  padding: 12px 32px 20rem;
 `;
