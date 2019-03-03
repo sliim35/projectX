2686a05c77dd3e758ae021dba10abdc7b5986015
sliim35 <slim.r35@gmail.com>
2019-03-03 14:17:56 +0300

    Footer component

---
+++ frontend/source/layout/footer/styles/FooterStyled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const FooterStyled = styled.footer`
+  width: 100%;
+  margin-left: 240px;
+  height: 36px;
+  border-top: 1px solid ${(props) => props.theme.borderColor};
+`;
