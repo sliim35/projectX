79aa5a61ee39f01c033e43074f77e0dba912d54e
sliim35 <slim.r35@gmail.com>
2019-03-03 14:16:12 +0300

    App component

---
+++ frontend/source/layout/app/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { ThemeProvider } from 'styled-components';
+
+import { Body } from '../body/Body';
+
+import { GlobalStyle } from '../../styles/global.js';
+import { theme } from '../../styles/theme';
+
+export const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        <Body />
+      </>
+    </ThemeProvider>
+  );
+};
--- frontend/source/styles/theme.js
+++ frontend/source/styles/theme.js
@@ -1,5 +1,15 @@
+const white = '#fff';
+const lightGrey = '#eee';
+const black = 'rgba(0,0,0, .56)';
+
 export const theme = {
-  white: '#FFF',
-  borderGrey: '#EEE',
-  navGrey: '#F8F8FA',
+  hoverWhite: white,
+  hoverBlack: black,
+  bodyBackgroundColor: white,
+  navBarColor: white,
+  navBarShadowColor: '1px 2px 4px 0 rgba(0,0,0,.03)',
+  borderColor: lightGrey,
+  sideBarColor: '#f8f8fa',
+  textColor: '#676767',
+  titleColor: '#444',
 };
